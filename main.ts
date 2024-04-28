0a3d1d1fa4c6aa22a5d1e4c835b371955a57e764
Qurat ul Ain <160419588+Qurat-01@users.noreply.github.com>
2024-04-28 09:46:37 -0700

    currency converter

---
+++ main.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.blue.bold("\n \tWelcome to currency converter\t \n"));
+
+const currency : any = {
+USD : 1, // base currency
+EUR : 0.91,
+GBP : 0.76,
+INR : 74.57,
+PKR : 200
+};
+let user_ans = await inquirer.prompt(
+    [
+{
+  name : "from",
+  message :chalk.green( "Enter from currency"),
+  type : "list",
+  choices : ["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name : "to",
+    message : chalk.green("Enter to currency"),
+    type : "list",
+    choices : ["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name : "amount",
+    message :chalk.blue( "Enter your amount"),
+    type : "number"
+}
+    ]
+)
+     let fromAmount = currency[user_ans.from]
+     let toAmount = currency[user_ans.to]
+     let amount = user_ans.amount 
+     let baseAmount = amount / fromAmount
+     let convertedAmount = baseAmount * toAmount
+
+     console.log(chalk.green(`converted Amount = ${chalk.yellow(convertedAmount.toFixed(1))} `)) ;
+     console.log(chalk.blue.bold("\n \tThanks for using!😊\t \n "))
